3f2271347b3a382f7f2dd546a5ebd84b333d9993
Greg V <greg@unrelenting.technology>
2018-04-16 21:02:24 +0300

    Drop nix dependency

--- src/result.rs
+++ src/result.rs
@@ -1,31 +1,25 @@
-use nix;
 use std::{io, result};
 
 #[derive(Debug)]
-pub struct Error(io::Error);
+pub enum Error {
+    IoError(io::Error),
+    Timeout,
+    Parse,
+}
 
 impl Into<io::Error> for Error {
     fn into(self) -> io::Error {
         match self {
-            Error(e) => e,
+            Error::IoError(e) => e,
+            Error::Timeout => io::Error::new(io::ErrorKind::Other, "devd poll timeout"),
+            Error::Parse => io::Error::new(io::ErrorKind::Other, "devd parse error"),
         }
     }
 }
 
 impl From<io::Error> for Error {
     fn from(err: io::Error) -> Error {
-        Error(err)
-    }
-}
-
-impl From<nix::Error> for Error {
-    fn from(err: nix::Error) -> Error {
-        Error(match err {
-            nix::Error::Sys(errno) => io::Error::from_raw_os_error(errno as i32),
-            nix::Error::InvalidPath => io::Error::new(io::ErrorKind::InvalidInput, "Invalid path"),
-            nix::Error::InvalidUtf8 => io::Error::new(io::ErrorKind::InvalidData, "Invalid UTF-8"),
-            nix::Error::UnsupportedOperation => io::Error::new(io::ErrorKind::Other, "Unsupported operation"),
-        })
+        Error::IoError(err)
     }
 }
 
